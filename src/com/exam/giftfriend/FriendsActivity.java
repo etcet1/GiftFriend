c46dae145b17adabb39f8913fd488d073c50ad5d
etcet1 <e_grekov@abv.bg>
2014-10-16 23:40:42 -0700

    activities switched to use the common base class

--- src/com/exam/giftfriend/FriendsActivity.java
+++ src/com/exam/giftfriend/FriendsActivity.java
@@ -19,7 +19,7 @@ import com.exam.giftfriend.sqlite.SQLiteRepo;
 import com.exam.giftfriend.sqlite.models.Friend;
 
 
-public class FriendsActivity extends Activity implements OnClickListener{
+public class FriendsActivity extends BaseActivity implements OnClickListener{
 	private SQLiteRepo friendsEntry;
 	private TextView contactsView;
 	private Button importBtn;
@@ -40,49 +40,6 @@ public class FriendsActivity extends Activity implements OnClickListener{
         
         importBtn.setOnClickListener(this);
         addBtn.setOnClickListener(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-        	//Toast.makeText(this, "Clicked!" + id, Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (id == R.id.home_button) {
-        	//Toast.makeText(this, "Clicked!" + id, Toast.LENGTH_SHORT).show();
-            Intent mainActivityIntent = new Intent(this, MainActivity.class);
-            startActivityForResult(mainActivityIntent, 0);
-            return true;
-        } else if (id == R.id.friends_button) {
-        	//Toast.makeText(this, "Clicked!" + id, Toast.LENGTH_SHORT).show();
-            Intent mainActivityIntent = new Intent(this, FriendsActivity.class);
-            startActivityForResult(mainActivityIntent, 0);
-            return true;
-        }else if (id == R.id.gifts_button) {
-        	Intent mainActivityIntent = new Intent(this, GiftsActivity.class);
-            startActivityForResult(mainActivityIntent, 0);
-            return true;
-		}
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void onBackPressed() {
-        if (getFragmentManager().getBackStackEntryCount() > 0) {
-            getFragmentManager().popBackStack();
-        } else {
-            // Default action on back pressed
-            super.onBackPressed();
-        }
     }
 
     private void recordContactsInDatabase(SQLiteRepo entry) {
--- src/com/exam/giftfriend/GiftsActivity.java
+++ src/com/exam/giftfriend/GiftsActivity.java
@@ -21,7 +21,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class GiftsActivity  extends Activity implements OnClickListener{
+public class GiftsActivity  extends BaseActivity implements OnClickListener{
 	
 	Button btnShowMine;
 	Button btnShowAll;
@@ -37,49 +37,7 @@ public class GiftsActivity  extends Activity implements OnClickListener{
         btnAdd.setOnClickListener(this);
         btnShowMine.setOnClickListener(this);
     }
-	
-	public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-	
-	@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-        	//Toast.makeText(this, "Clicked!" + id, Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (id == R.id.home_button) {
-        	///Toast.makeText(this, "Clicked!" + id, Toast.LENGTH_SHORT).show();
-            Intent mainActivityIntent = new Intent(this, MainActivity.class);
-            startActivityForResult(mainActivityIntent, 0);
-            return true;
-        } else if (id == R.id.friends_button) {
-        	//Toast.makeText(this, "Clicked!" + id, Toast.LENGTH_SHORT).show();
-            Intent mainActivityIntent = new Intent(this, FriendsActivity.class);
-            startActivityForResult(mainActivityIntent, 0);
-            return true;
-        } else if (id == R.id.gifts_button) {
-        	Intent mainActivityIntent = new Intent(this, GiftsActivity.class);
-            startActivityForResult(mainActivityIntent, 0);
-            return true;
-		}
-        return super.onOptionsItemSelected(item);
-    }
 
-    /*public void onBackPressed() {
-        if (getFragmentManager().getBackStackEntryCount() > 0) {
-            getFragmentManager().popBackStack();
-        } else {
-            // Default action on back pressed
-            super.onBackPressed();
-        }
-    }*/
-    
     public void onClick(View v) {
 		//btn_my_gifts -> show all gifts from SQLite
 		//btn_public_gifts -> show all gifts from public base
